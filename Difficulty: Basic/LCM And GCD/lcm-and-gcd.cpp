5c1b160b6c43150e488374d1bba1ec7310035577
Vartika Singh <154607046+buzzcodez@users.noreply.github.com>
2025-03-25 23:44:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/LCM And GCD/lcm-and-gcd.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    vector<int> lcmAndGcd(int a, int b) {
+        // code here
+        int oria=a , orib=b;
+        while(a>0 and b>0){
+            if(a>b){
+                a=a%b;
+            }
+            else{
+                b=b%a;
+            }
+        }
+        int gcd=max(a,b);
+        int lcm=(oria/gcd)*orib;
+        return{lcm,gcd};
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        int A, B;
+
+        cin >> A >> B;
+
+        Solution ob;
+        vector<int> ans = ob.lcmAndGcd(A, B);
+        cout << ans[0] << " " << ans[1] << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
