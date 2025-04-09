47521e1bd89a71e0bde39026998ebb7aa5081576
Vartika Singh <154607046+buzzcodez@users.noreply.github.com>
2025-04-09 17:19:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/LCM And GCD/lcm-and-gcd.cpp
+++ Difficulty: Basic/LCM And GCD/lcm-and-gcd.cpp
@@ -6,19 +6,31 @@ using namespace std;
 // } Driver Code Ends
 
 class Solution {
+    private:
+    int GCD(int a,int b){
+        while(a>0 and b>0){
+            if(a>b){
+                a=a%b;
+            }
+            else{
+                b=b%a;
+            }
+           
+        }
+         if(a==0){
+                return b;
+            }
+            return a;
+    }
+    int LCM(int a,int b){
+    int gcd= GCD(a,b);
+    return (a*b)/gcd;
+    }
   public:
     vector<int> lcmAndGcd(int a, int b) {
         // code here
-        int oria=a , orib=b;
-        while(a>0 and b>0){
-            if(a>b)
-             a=a%b;
-            else
-             b=b%a;
-        }
-        int gcd=max(a,b);
-        
-        int lcm = (oria/gcd)*orib;
+       int gcd= GCD(a,b);
+        int lcm=LCM(a,b);
         return{lcm,gcd};
     }
 };
