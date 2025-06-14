865c1c727c9a8755126eed6f830a5441ee775602
Vartika Singh <154607046+vartikasinghp@users.noreply.github.com>
2025-06-14 23:22:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Palindrome String/palindrome-string.cpp
+++ Difficulty: Easy/Palindrome String/palindrome-string.cpp
@@ -1,52 +1,20 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-// User function template for C++
-
 class Solution {
   public:
     // Function to check if a string is a palindrome.
     bool isPalindrome(string& s) {
-        // get 20lpa before prafull
-        int i=0 , j=s.length()-1;
-        while(i<j){
-            if(s[i]!=s[j]){
-                return false;
+        // code here
+        int st=0;
+        int e=s.size()-1;
+        while(st<=e){
+            if(s[st]!=s[e]){
+                return 0;
             }
-            i++;
-            j--;
+            else{
+                st++;
+                e--;
+            }
         }
-        return true;
+        return 1;
         
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    int t;
-    cin >> t; // Read the number of test cases
-    while (t--) {
-        string s;
-        cin >> s; // Read the string
-
-        Solution ob; // Create an instance of Solution
-        if (ob.isPalindrome(s)) {
-            cout << "true"; // Print true if palindrome
-        } else {
-            cout << "false"; // Print false if not a palindrome
-        }
-        cout << "\n~\n"; // Print newline after each result
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
